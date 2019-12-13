d16fd808513cdceeb84821f4b789d25cee271619
guilhembn <gbuisan@laas.fr>
2019-12-13 09:23:08 +0100

    Updates main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ mod clustering;
 mod geometrical_tools;
 
 pub use crate::clustering::clusterer;
+pub use lidar_rd::Sample;
 
 fn main() {
-    let _c = clusterer::Cluster::new();
+    let s = Sample{angle: 2.34, distance: 345, quality: 13};
+    let _c = clusterer::Cluster::new(&s);
+
 }
