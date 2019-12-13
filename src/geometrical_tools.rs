ee5fcb7590dae712de84b5c1b4294e724b4e5ddb
guilhembn <gbuisan@laas.fr>
2019-12-13 09:29:24 +0100

    Adds unit tests

--- src/geometrical_tools.rs
+++ src/geometrical_tools.rs
@@ -40,4 +40,27 @@ pub fn wrap_angle(angle: f64) -> f64{
 
     return a;
 }
+
+#[cfg(test)]
+mod test{
+    use super::*;
+
+    #[test]
+    fn test_new_polar_point(){
+        let pp = PolarPoint::new(235.5, 2.45);
+        let pp2 = PolarPoint::new(365.35, 6.0 * PI);
+        assert_eq!(pp.distance, 235.5);
+        assert_eq!(pp.angle, 2.45);
+
+        assert_eq!(pp2.distance, 365.35);
+        assert_eq!(pp2.angle, 0.0);
+    }
+
+    #[test]
+    fn test_wrap_angle(){
+        assert_eq!(wrap_angle(1.23), 1.23);
+        assert_eq!(wrap_angle(6.0 * PI), 0.0);
+        assert!(wrap_angle(-45.0 * PI) - (-PI) < 10.0f64.powi(-8));
+    }
+
 }
