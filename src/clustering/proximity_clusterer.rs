7d50912580100c8618b9ccffa862057c9e64cde9
guilhembn <gbuisan@laas.fr>
2020-06-01 14:51:06 +0200

    Adds max angle on proximity clusterer

--- src/clustering/proximity_clusterer.rs
+++ src/clustering/proximity_clusterer.rs
@@ -1,8 +1,10 @@
 use super::clusterer::{Clusterer, Cluster};
 use lidar_rd::Sample;
+use crate::geometrical_tools::wrap_angle;
 
 pub struct ProximityCluster{
-    pub maximal_distance: f64
+    pub maximal_distance: f64,
+    pub maximal_angle: f64  // If two following points, less than maximal_distance are more than this angle apart, still create a new group
 }
 
 impl Clusterer for ProximityCluster{
@@ -18,11 +20,18 @@ impl Clusterer for ProximityCluster{
                 continue;
             }
             let s = sample.unwrap();
-            if last_point.is_none() || (last_point.unwrap().distance as f64 - s.distance as f64).abs() >= self.maximal_distance {
+            if last_point.is_none() {
+                // First cluster
                 clusters.push(Cluster::new(&s));
             }else{
-                let n = clusters.len();
-                clusters[n-1].push(&s);
+                let p = last_point.unwrap();
+                if (p.distance as f64 - s.distance as f64).abs() >= self.maximal_distance 
+                        || wrap_angle(p.angle - s.angle).abs() >= self.maximal_angle{
+                    clusters.push(Cluster::new(&s));
+                }else{
+                    let n = clusters.len();
+                    clusters[n-1].push(&s);
+                }
             }
             last_point = Some(s); // Copy...
         }
@@ -43,11 +52,12 @@ mod tests{
         let s3 = Some(Sample{angle: 2.36, distance: 338, quality: 13});
         let s4 = Some(Sample{angle: 2.37, distance: 218, quality: 6});
         let s5 = Some(Sample{angle: 2.38, distance: 225, quality: 9});
-        let v = vec!(s1, s2, s3, s4, s5);
+        let s6 = Some(Sample{angle: 2.50, distance: 225, quality: 10});  // Maximal angle test
+        let v = vec!(s1, s2, s3, s4, s5, s6);
         let ov = Some(v);
-        let pc = ProximityCluster{maximal_distance: 50.0};
+        let pc = ProximityCluster{maximal_distance: 50.0, maximal_angle: 0.1};
         let clusters = pc.cluster(&ov).unwrap();
-        assert_eq!(clusters.len(), 2);
+        assert_eq!(clusters.len(), 3);
         assert_eq!((2.35 - clusters[0].barycenter.angle).abs() <= 0.000001, true);
         assert_eq!(clusters[0].barycenter.distance, 341.);
         assert_eq!(clusters[0].max_intensity, 13);
@@ -61,6 +71,13 @@ mod tests{
         assert_eq!(clusters[1].points.len(), 2);
         assert_eq!(clusters[1].closest_point.angle, 2.37);
         assert_eq!(clusters[1].closest_point.distance, 218.);
+
+        assert_eq!(clusters[2].barycenter.angle, 2.50);
+        assert_eq!(clusters[2].barycenter.distance, 225.);
+        assert_eq!(clusters[2].max_intensity, 10);
+        assert_eq!(clusters[2].points.len(), 1);
+        assert_eq!(clusters[2].closest_point.angle, 2.50);
+        assert_eq!(clusters[2].closest_point.distance, 225.);
     }
 }
 
